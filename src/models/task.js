68b87b839957ea7cf79c6ec494a0a16bf2fa8631
jeessyy169627-cloud <jeessyy169627@gmail.com>
2026-02-09 11:42:31 -0600

    Delete src/models/task.js

--- src/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const taskSchema = new Schema ({
-    title: String,
-    description: String
-});
-
-module.exports = mongoose.model('Task', taskSchema);
